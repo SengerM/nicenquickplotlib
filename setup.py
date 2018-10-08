42ba86b4556c14061f0be523f282067f3fb7aa94
Matias Senger <m.senger@hotmail.com>
2018-10-08 01:49:29 -0300

    Corrected package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
 	long_description = fh.read()
 
 setuptools.setup(
-	name="nicenquick",
+	name="nicenquickplotlib",
 	version="0.0.1",
 	author="Matias Senger",
 	author_email="m.senger@hotmail.com",
