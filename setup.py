bebddbdc74d166cbd9a8659dcab534807b8f8ed5
Matias Senger <m.senger@hotmail.com>
2018-10-11 00:58:57 -0300

    Corrected version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name="nicenquickplotlib",
-	version="0.0.1",
+	version="0.0.2",
 	author="Matias Senger",
 	author_email="m.senger@hotmail.com",
 	description="Package to produce nice plots quickly using matplotlib",
