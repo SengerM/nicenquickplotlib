ba7e7b251b75a8e67c251e6934ccd147ef2902bb
Matias Senger <m.senger@hotmail.com>
2018-10-11 00:47:29 -0300

    Corrected file naming

--- test.py
+++ test.py
@@ -12,6 +12,6 @@ nq.plot([y1,y2,y3])
 nq.plot(x1, y1)
 nq.plot(x1, [y1,y2,y3])
 nq.plot([x1,x2], [np.sin(x1), np.sin(x2)], marker='.')
-nq.plot([x1,x2], [np.sin(x1), np.sin(x2)], together=False, marker='.')
+nq.plot([x1,x2], [np.sin(x1), np.sin(x2)], together=False, marker='.', title='My beautifull plot')
 
 nq.save_all(csv=True, timestamp=True)
