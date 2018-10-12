72e2751dae4892e4a5ef271d9b867c87a63214d1
Matias Senger <m.senger@hotmail.com>
2018-10-12 20:15:24 -0300

    The "default config params" must not be accessible from outside. This needs a better implementation. As is now, this will not work anymore.

--- nicenquickplotlib/default_configuration_params.py
+++ nicenquickplotlib/default_configuration_params.py
@@ -1,12 +1,12 @@
 import numpy as np
 # Configure your plots -------------
-default_file_format = 'png'
-default_save_directory = 'figures'
-default_colors = np.array([(255, 102, 0), (51, 153, 255), (51, 102, 0), (255, 0, 255)])/255
-default_fig_width = 160e-3 # Figure width in meters (size of the chart+axis_ticks, not counting legend, title, etc.).
-default_fig_ratio = [1, 0.6] # XY ratio aspect of figures.
-default_hspace = 0.1 # Vertical separation between subplots.
-default_dpi_rasterization = 200 # Resolution for bitmap format.
+__default_file_format = 'png'
+__default_save_directory = 'figures'
+__default_colors = np.array([(255, 102, 0), (51, 153, 255), (51, 102, 0), (255, 0, 255)])/255
+__default_fig_width = 160e-3 # Figure width in meters (size of the chart+axis_ticks, not counting legend, title, etc.).
+__default_fig_ratio = [1, 0.6] # XY ratio aspect of figures.
+__default_hspace = 0.1 # Vertical separation between subplots.
+__default_dpi_rasterization = 200 # Resolution for bitmap format.
 def default_grid(ax):
 	ax.grid(b=True, which='major', color='#000000', alpha=0.3, linestyle='-', linewidth=0.5)
 	ax.grid(b=True, which='minor', color='#000000', alpha=0.15, linestyle='-', linewidth=0.25)
